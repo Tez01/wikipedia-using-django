3b2c70548660ad81d9af846c78539cdf65ec3de5
Tej <tejpartap02@gmail.com>
2022-01-18 12:18:52 -0500

    get_entries error fix

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -6,28 +6,17 @@ import markdown
 from django.http import HttpResponse
 
 from . import util
-from django.core.files.storage import default_storage
 
-def get_entry(title):
-    """
-    Retrieves an encyclopedia entry by its title. If no such
-    entry exists, the function returns None.
-    """
-    try:
-        f = default_storage.open(f"entries/{title}.md")
-        return f.read().decode("utf-8")
-    except FileNotFoundError:
-        return None
 def index(request):
     # Check if method is post
     if request.method == "POST":
         title = request.POST.get("q")
 
         #Fetch the entry
-        entry = get_entry(title)
+        entry = util.get_entry(title)
 
         #Check if entry is valid
-        if get_entry(title) != None:
+        if util.get_entry(title) != None:
 
             #Create html from markdown
             html = markdown.markdown(entry)
