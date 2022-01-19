653cb64e1979956ff1dc3a8dbe2ca1ca173ea792
Tej <tejpartap02@gmail.com>
2022-01-19 11:49:52 -0500

    Fix some issues with search box

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,24 +10,25 @@ from . import util
 def index(request):
     # Check if method is post
     if request.method == "POST":
+        title = None
         if 'q' in request.POST:
             title = request.POST.get("q")
         elif 'submit_param' in request.POST:
             title = request.POST.get("submit_param")
-            #Fetch the entry
-            entry = util.get_entry(title)
+        #Fetch the entry
+        entry = util.get_entry(title)
 
-            #Check if entry is valid
-            if util.get_entry(title) != None:
+        #Check if entry is valid
+        if util.get_entry(title) != None:
 
-                #Create html from markdown
-                html = markdown.markdown(entry)
-                return HttpResponse(f"{html}")
-            else:
+            #Create html from markdown
+            html = markdown.markdown(entry)
+            return HttpResponse(f"{html}")
+        else:
 
-                #Print error
-                return HttpResponse("<h1>The page does not exist</h1>")
-        
+            #Print error
+            return HttpResponse("<h1>The page does not exist</h1>")
+    
             
 
     return render(request, "encyclopedia/index.html", {
